ab13c9b499fe4eed3695a5b80136738dc05c1fae
Martin Waller <martin@Martins-MacBook-Pro.local>
2018-03-09 16:43:23 +0000

    First naive version without tests

---
+++ src/main/java/phrases/PhraseController.java
@@ -0,0 +1,16 @@
+package phrases;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PhraseController {
+
+    @RequestMapping()
+    public PhraseSearch ps(@RequestParam(value="text") String text) throws IOException {
+        return new PhraseSearch(text);
+    }
+}
